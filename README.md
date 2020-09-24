eb37a26cc21b10a479321ed2cdded88a24310794
Wayne <w.p.lam107@gmail.com>
2020-09-24 19:14:56 -0400

    updates

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # NBA Analyses
 
+Data:
+- NBA API: https://github.com/swar/nba_api
+
 Notebooks:
 - ```pps_init_eda.ipynb```: Points per shot for midrange and 3 point shots based on closest defender distance
 - ```pps_seasons.ipynb```: Points per shot by season
--- app/app.py
+++ app/app.py
@@ -1,2 +1,16 @@
-import numpy as np
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import plotly.graph_objects as go
+
+from dash.dependencies import Input, Output
+
 import pandas as pd
+import numpy as np
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+
